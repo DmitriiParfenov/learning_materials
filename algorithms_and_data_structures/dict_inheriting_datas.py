28c92e03b3857cb50004cc3a7e57a2db3c60570b
Dmitrii <dima12sik@yandex.ru>
2023-04-05 06:15:48 +0300

    add get_dict_inhere_func

---
+++ algorithms_and_data_structures/dict_inheriting_datas.py
@@ -0,0 +1,18 @@
+def get_dict_inhere(structure, parent, child):
+    """
+    Функция реализует структуру данных, которая отражает наследование объектов.
+    Structure — это словарь, ключами которого являются объекты, а значения этох ключей  — это
+    список объектов, унаследованных от объекта ключа
+    :param structure: dict
+    :param parent: hashable object
+    :param child: list
+    :return: dict
+    """
+    if parent not in structure:
+        structure[parent] = []
+        structure[parent].extend(child)
+        for elem in structure[parent]:
+            if elem in structure:
+                structure[parent].extend(structure[elem])
+        structure[parent] = set(structure[parent])
+        return structure
