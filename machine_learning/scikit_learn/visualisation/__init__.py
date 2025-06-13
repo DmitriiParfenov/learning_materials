7333b401aaf5cafb4a202ab9d2c72022151c1248
Dmitrii <dima12sik@yandex.ru>
2025-06-13 13:57:18 +0300

    updated imports

--- machine_learning/scikit_learn/visualisation/__init__.py
+++ machine_learning/scikit_learn/visualisation/__init__.py
@@ -1,5 +1,19 @@
-__all__ = (
-    'plot_decision_regions'
+from machine_learning.scikit_learn.visualisation.base_visualisation import plot_decision_regions
+from machine_learning.scikit_learn.visualisation.lda_visualisation import (
+    visualize_discriminants,
+    visualize_new_lda_frame,
+)
+from machine_learning.scikit_learn.visualisation.pca_visualisation import (
+    visualize_dispersions,
+    visualize_new_pca_frame,
+    visualize_features_impacts_for_pca
 )
 
-from .base_visualisation import plot_decision_regions
+__all__ = (
+    'plot_decision_regions',
+    'visualize_dispersions',
+    'visualize_new_pca_frame',
+    'visualize_features_impacts_for_pca',
+    'visualize_discriminants',
+    'visualize_new_lda_frame',
+)
