bbfa13acc62df33ef91d65f653abb3e93f08956a
Dmitrii <dima12sik@yandex.ru>
2025-06-15 18:21:41 +0300

    updated imports

--- machine_learning/scikit_learn/visualisation/__init__.py
+++ machine_learning/scikit_learn/visualisation/__init__.py
@@ -3,10 +3,16 @@ from machine_learning.scikit_learn.visualisation.lda_visualisation import (
     visualize_discriminants,
     visualize_new_lda_frame,
 )
+from machine_learning.scikit_learn.visualisation.learning_curve import (
+    visualize_learning_curve
+)
 from machine_learning.scikit_learn.visualisation.pca_visualisation import (
     visualize_dispersions,
     visualize_new_pca_frame,
     visualize_features_impacts_for_pca
+)
+from machine_learning.scikit_learn.visualisation.validation_curve import (
+    visualize_validation_curve
 )
 
 __all__ = (
@@ -16,4 +22,6 @@ __all__ = (
     'visualize_features_impacts_for_pca',
     'visualize_discriminants',
     'visualize_new_lda_frame',
+    'visualize_learning_curve',
+    'visualize_validation_curve',
 )
