3f15976f60d38517d09e09ac9158d0b34c064a46
Dmitrii <dima12sik@yandex.ru>
2025-07-31 20:04:06 +0300

    updated imports

---
+++ machine_learning/scikit_learn/regression/utils/__init__.py
@@ -0,0 +1,5 @@
+from .prepare_data import prepared_ames_housing_data
+
+__all__ = (
+    'prepared_ames_housing_data',
+)
---
+++ machine_learning/scikit_learn/regression/visualisation/__init__.py
@@ -0,0 +1,13 @@
+from .residual_plot import visualize_residual_plot
+from .statistics import (
+    anova_results,
+    get_statistics_result,
+    get_accuracy_scores,
+)
+
+__all__ = (
+    'visualize_residual_plot',
+    'anova_results',
+    'get_statistics_result',
+    'get_accuracy_scores',
+)
