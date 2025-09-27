76a9d3ac835d21b4ba9a94b716ca8262558584ad
Dmitrii <dima12sik@yandex.ru>
2025-09-27 15:04:16 +0300

    added soft_max and tanh

--- machine_learning/deep_learning/utils/activations.py
+++ machine_learning/deep_learning/utils/activations.py
@@ -3,3 +3,11 @@ import numpy as np
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+def soft_max(z):
+    return np.exp(z) / (np.sum(np.exp(z)))
+
+def tanh(z):
+    e_p = np.exp(z)
+    e_m = np.exp(-z)
+    return (e_p - e_m) / (e_p + e_m)
