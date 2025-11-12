1956a558a75d5218e0141b7ceefaffc3e864cf22
Dmitrii <dima12sik@yandex.ru>
2025-11-13 00:30:51 +0300

    исправил минорные баги

--- machine_learning/rnn/imdb_project/preprocessing/upload_data.py
+++ machine_learning/rnn/imdb_project/preprocessing/upload_data.py
@@ -1,4 +1,3 @@
-import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
@@ -7,16 +6,16 @@ from machine_learning.rnn.preprocessing import SimpleTextVectorizer
 
 
 def load_data(batch_size: int = 32, device: str = 'cpu'):
-    torch.manual_seed(1)
     # Загрузили сырые данные.
     imdb = load_dataset('imdb')
     train_dataset, valid_dataset, test_dataset = get_datasets(imdb)
     # Получения словаря с токенами.
     vectorizer = SimpleTextVectorizer()
     vectorizer.fit(train_dataset)
+    vocabulary = vectorizer.get_vocabulary()
     # Получаем загрузчики данных.
-    collate_fn = IMDBCustomCollate(vectorizer.get_vocabulary(), vectorizer.get_tokenizer, device)
-    train_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
-    valid_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
+    collate_fn = IMDBCustomCollate(vocabulary, vectorizer.get_tokenizer, device)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False)
-    return train_dataloader, valid_dataloader, test_dataloader
+    return train_dataloader, valid_dataloader, test_dataloader, vocabulary
